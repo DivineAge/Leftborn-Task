2f1f14bb1c8cea21b6d25ab69e033649e44705ed
DivineAge <yussufawad0852@gmail.com>
2026-08-12 20:29:30 +0300

    Added exception for unhandled ones

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,15 +1,49 @@
+using API.Extensions;
+using API.Middleware;
+using Module.Users.Infrastructure;
 using System.Reflection;
+using Test.Common.Application;
+using Test.Common.Infrastructure;
+using Test.Common.Infrastructure.Configuration;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerDocumentation();
+
+
+Assembly[] moduleAssemblies = [
+    Module.Users.Application.AssemblyRefrence.Assembly
+];
+
+builder.Services.AddApplication(moduleAssemblies);
+
+string databaseConnectionString = builder.Configuration.GetConnectionStringOrThrow("Database");
+
+builder.Services.AddInfrastructure(databaseConnectionString);
+
+builder.Configuration.AddModuleConfiguration(["users", "events", "ticketing", "attendance"]);
+
+builder.Services.AddUsersModule(builder.Configuration);
 
 WebApplication app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    app.ApplyMigrations();
 }
 
-app.UseHttpsRedirection();
+app.UseExceptionHandler();
+
+
+app.Run();
+
 
 
 app.Run();
