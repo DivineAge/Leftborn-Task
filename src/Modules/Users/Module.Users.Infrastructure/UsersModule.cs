7527410f7ba39c393ee1a874835d2d0e13039e93
DivineAge <yussufawad0852@gmail.com>
2026-08-12 20:57:10 +0300

    added the users module infrastrcutre with ef core and repositories

---
+++ src/Modules/Users/Module.Users.Infrastructure/UsersModule.cs
@@ -0,0 +1,42 @@
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+using Module.Users.Application.Abstractions.Data;
+using Module.Users.Domain.Users;
+using Module.Users.Infrastructure.Database;
+using Module.Users.Infrastructure.Users;
+using Test.Common.Presentation.Endpoints;
+
+namespace Module.Users.Infrastructure;
+
+public static class UsersModule
+{
+    public static IServiceCollection AddUsersModule(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddInfrastructure(configuration);
+
+        services.AddEndPoints(Presentation.AssemblyReference.Assembly);
+
+        return services;
+    }
+    private static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+
+        services.AddDbContext<UsersDbContext>(options =>
+            options.UseNpgsql(
+                configuration.GetConnectionString("Database"),
+                    npgsqlOptions => npgsqlOptions
+                        .MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Users))
+            );
+
+        services.AddScoped<IUserRepository, UserRepository>();
+
+        services.AddScoped<IUnitOfWork, IUnitOfWork>();
+
+    }
+
+}
