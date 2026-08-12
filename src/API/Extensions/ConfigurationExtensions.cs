ba5c8c918246f09e7ff8224bd75092224945c445
DivineAge <yussufawad0852@gmail.com>
2026-08-12 20:26:26 +0300

    added extensions for swagger migrations and configuration handeling

---
+++ src/API/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,13 @@
+namespace API.Extensions;
+
+internal static class ConfigurationExtensions
+{
+    internal static void AddModuleConfiguration(this IConfigurationBuilder configurationBuilder, string[] modules)
+    {
+        foreach (string module in modules)
+        {
+            configurationBuilder.AddJsonFile($"module.{module}.json", false, true);
+            configurationBuilder.AddJsonFile($"module.{module}.Development.json", true, true);
+        }
+    }
+}
---
+++ src/API/Extensions/MigrationExtension.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Module.Users.Infrastructure;
+using Module.Users.Infrastructure.Database;
+
+namespace API.Extensions;
+
+public static class MigrationExtension
+{
+    public static void ApplyMigrations(this IApplicationBuilder app)
+    {
+        using IServiceScope scope = app.ApplicationServices.CreateScope();
+
+        ApplyMigration<UsersDbContext>(scope);
+    }
+    private static void ApplyMigration<TDbContext>(IServiceScope scope)
+    where TDbContext : DbContext
+    {
+        using TDbContext context = scope.ServiceProvider.GetRequiredService<TDbContext>();
+
+        context.Database.Migrate();
+    }
+}
