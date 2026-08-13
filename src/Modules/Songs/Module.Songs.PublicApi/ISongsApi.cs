8be0f977187629d3122f496f802db362963f8e85
DivineAge <yussufawad0852@gmail.com>
2026-08-13 20:25:56 +0300

    added public api for module communication for futrure use

---
+++ src/Modules/Songs/Module.Songs.PublicApi/ISongsApi.cs
@@ -0,0 +1,11 @@
+
+namespace Module.Songs.PublicApi;
+
+public interface ISongsApi
+{
+    Task CreatePublisherAsync(
+    Guid publisherId,
+    string firstName,
+    string lastName,
+    CancellationToken cancellationToken = default);
+}
