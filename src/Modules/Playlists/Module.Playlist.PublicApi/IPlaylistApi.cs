5657cf3e90ad436e8398c55d5984be543db0dbc3
DivineAge <yussufawad0852@gmail.com>
2026-08-14 22:59:43 +0300

    added the interface for the module communication

---
+++ src/Modules/Playlists/Module.Playlist.PublicApi/IPlaylistApi.cs
@@ -0,0 +1,21 @@
+
+
+namespace Module.Playlist.PublicApi;
+
+public interface IPlaylistApi
+{
+    Task CreateUserAsync(
+    Guid userId,
+    string firstName,
+    string lastName,
+    CancellationToken cancellationToken = default);
+
+    Task CreateSongAsync(
+    Guid songId,
+    Guid publisherId,
+    int timeInSeconds,
+    string name,
+    CancellationToken cancellationToken = default);
+
+
+}
