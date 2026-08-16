40b57e90e96fa1f858f45b438dff2eecbdb9b890
DivineAge <yussufawad0852@gmail.com>
2026-08-16 17:24:23 +0300

    Updated UserError to UserErrors for consistency

--- src/Modules/Users/Module.Users.Domain/Users/UserErrors.cs
+++ src/Modules/Users/Module.Users.Domain/Users/UserErrors.cs
@@ -2,7 +2,7 @@ using Test.Common.Domain;
 
 namespace Module.Users.Domain.Users;
 
-public class UserError
+public class UserErrors
 {
     public static Error NotFound(Guid userId) => Error.NotFound("Users.NotFound", $"The user with the identifier {userId} not found");
     public static Error EmailAlreadyExists(string email) => Error.Conflict("Users.EmailAlreadyExists", $"The user with the email {email} already exists");
