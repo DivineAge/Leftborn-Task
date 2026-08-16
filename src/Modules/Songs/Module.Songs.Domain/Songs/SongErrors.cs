ca1d1ddce2572b1471dd2e5113decb90b6de4e7b
DivineAge <yussufawad0852@gmail.com>
2026-08-16 17:22:34 +0300

    changed UserSongError to UserSongErrors for consistency

--- src/Modules/Songs/Module.Songs.Domain/Songs/SongErrors.cs
+++ src/Modules/Songs/Module.Songs.Domain/Songs/SongErrors.cs
@@ -4,7 +4,7 @@ using Test.Common.Domain;
 
 namespace Module.Songs.Domain.Songs;
 
-public static class SongError
+public static class SongErrors
 {
     public static Error NotFound(Guid songId) => Error.NotFound("Song.NotFound", $"Song with id: {songId} not found");
     public static Error NotFound(string name) => Error.NotFound("Song.NotFound", $"Song with name: {name} not found");
